9a7e29f6d808afd15650ab69789f6f06c226452d
anca26 <anca.iacob26@gmail.com>
2025-04-15 16:24:17 +0300

    Items list

--- custom_app/lib/main.dart
+++ custom_app/lib/main.dart
@@ -30,7 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
+   
   List pages = [
     const HomePage(),
   ];
@@ -38,11 +38,11 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.lightBlue,
+      //   title: Text(widget.title),
+      //   centerTitle: true,
+      // ),
       body:  const HomePage()
     );
   }
--- custom_app/lib/pages/home_page.dart
+++ custom_app/lib/pages/home_page.dart
@@ -9,23 +9,54 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>{
   // tbi: final List<Product> products = [];
+  Color backgroundGray = const Color.fromARGB(255, 244, 244, 244);
 
   @override 
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: backgroundGray,
       body: SafeArea(child: Padding (
-        padding: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextField(
               decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
                 hintText: 'Search product', 
                 prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide.none
+                  ),
+                  
               ),
             ),
-            //tbi SizedBox(height:8.0),
+            SizedBox(height:8.0),
+            Expanded(
+              child: GridView.builder(
+                itemCount: 12, //products.length after fetching 
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 1,
+                  ),
+                itemBuilder: (context, index) {
+                  return Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Text(
+                      'Item ${index +1}',
+                    ),
+                  );
+                },
+              ),
+            ),
           ]
         ))
         ),
